95c6fbc3c9f758b8e9468313fbba748049391e73
MrPierre20 <mrpierre007@gmail.com>
2020-09-05 20:14:33 +0200

    Adding start.bat

---
+++ start.bat
@@ -0,0 +1,45 @@
+@echo off
+TITLE Insta stonks by UwU#0001 (read please)
+color c
+echo " /$$$$$$                       /$$                /$$$$$$   /$$                         /$$                
+echo "|_  $$_/                      | $$               /$$__  $$ | $$                        | $$                
+echo "  | $$   /$$$$$$$   /$$$$$$$ /$$$$$$    /$$$$$$ | $$  \__//$$$$$$    /$$$$$$  /$$$$$$$ | $$   /$$  /$$$$$$$
+echo "  | $$  | $$__  $$ /$$_____/|_  $$_/   |____  $$|  $$$$$$|_  $$_/   /$$__  $$| $$__  $$| $$  /$$/ /$$_____/
+echo "  | $$  | $$  \ $$|  $$$$$$   | $$      /$$$$$$$ \____  $$ | $$    | $$  \ $$| $$  \ $$| $$$$$$/ |  $$$$$$ 
+echo "  | $$  | $$  | $$ \____  $$  | $$ /$$ /$$__  $$ /$$  \ $$ | $$ /$$| $$  | $$| $$  | $$| $$_  $$  \____  $$
+echo " /$$$$$$| $$  | $$ /$$$$$$$/  |  $$$$/|  $$$$$$$|  $$$$$$/ |  $$$$/|  $$$$$$/| $$  | $$| $$ \  $$ /$$$$$$$/
+echo "|______/|__/  |__/|_______/    \___/   \_______/ \______/   \___/   \______/ |__/  |__/|__/  \__/|_______/ 
+color c
+echo "
+echo "
+echo "  Make sure Java & Chrome (ver. 85) is installed on your computer
+echo "  If Java is installed press ENTER , if its not, close this window and install it
+pause
+cls
+TITLE Insta stonks by UwU#0001 (accept terms)
+color 6
+echo " /$$$$$$                       /$$                /$$$$$$   /$$                         /$$                
+echo "|_  $$_/                      | $$               /$$__  $$ | $$                        | $$                
+echo "  | $$   /$$$$$$$   /$$$$$$$ /$$$$$$    /$$$$$$ | $$  \__//$$$$$$    /$$$$$$  /$$$$$$$ | $$   /$$  /$$$$$$$
+echo "  | $$  | $$__  $$ /$$_____/|_  $$_/   |____  $$|  $$$$$$|_  $$_/   /$$__  $$| $$__  $$| $$  /$$/ /$$_____/
+echo "  | $$  | $$  \ $$|  $$$$$$   | $$      /$$$$$$$ \____  $$ | $$    | $$  \ $$| $$  \ $$| $$$$$$/ |  $$$$$$ 
+echo "  | $$  | $$  | $$ \____  $$  | $$ /$$ /$$__  $$ /$$  \ $$ | $$ /$$| $$  | $$| $$  | $$| $$_  $$  \____  $$
+echo " /$$$$$$| $$  | $$ /$$$$$$$/  |  $$$$/|  $$$$$$$|  $$$$$$/ |  $$$$/|  $$$$$$/| $$  | $$| $$ \  $$ /$$$$$$$/
+echo "|______/|__/  |__/|_______/    \___/   \_______/ \______/   \___/   \______/ |__/  |__/|__/  \__/|_______/ 
+echo "
+echo "
+echo "  By pressing ENTER again you take your own responsability if your account get banned or restricted.
+echo "  If you wont take the risk, close this program.
+pause
+color a
+TITLE Insta stonks by UwU#0001 (Staring ...)
+ping 192.0.2.1 -n 1 -w 700 >nul
+ping 192.0.2.1 -n 1 -w 700 >nul
+ping 192.0.2.1 -n 1 -w 700 >nul
+ping 192.0.2.1 -n 1 -w 700 >nul
+cls
+TITLE Insta stonks by UwU#0001
+color b
+cd core
+java -jar InstaStonks.jar
+pause
