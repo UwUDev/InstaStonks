b3552839ecdb6fdd576e39a9342e7782d5e64626
UwUDev <mmimi0593@gmail.com>
2020-11-01 18:00:27 +0100

    Config detection

--- src/main/java/me/uwu/Main.java
+++ src/main/java/me/uwu/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         ConfigUtils.setupConfigs();
+        ConfigUtils.getConfigsName();
         Discord.startup();
         Discord.update(-1, null);
         launch(args);
--- src/main/java/me/uwu/config/ConfigUtils.java
+++ src/main/java/me/uwu/config/ConfigUtils.java
@@ -1,9 +1,12 @@
 package me.uwu.config;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class ConfigUtils {
 
@@ -19,6 +22,28 @@ public class ConfigUtils {
                 System.err.println("Failed to create directory!" + e.getMessage());
             }
         }
+    }
+
+    public static ArrayList<String> getConfigsName(){
+        ArrayList<String> result = new ArrayList<String>();
+        try {
+            File f = new File("config");
+
+            FilenameFilter filter = (f1, name) -> name.endsWith(".OwO");
+
+            File[] files = f.listFiles(filter);
+
+            for (File file : files) {
+                result.add(file.getName().replaceFirst("[.][^.]+$", ""));
+            }
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+        for (String oof : result ){
+            System.out.println(oof);
+        }
+        return result;
     }
 
 }
